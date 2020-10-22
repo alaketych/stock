89516cd24731a97543be59f615beb4ecaf0e1816
alaketych <alaketych@ukr.net>
2020-10-22 17:20:21 +0300

    api routing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const router =  express.Router()
 
-router.get('/', (request, response) => {
-    response.send('it works')
-})
+const wareController = require('../controllers/ware')
+const providorController = require('../controllers/providor')
+const categoryController = require('../controllers/category')
+
+//display all items
+router.get('/', wareController.getWares)
+router.get('/providors', providorController.getProvidors)
+router.get('/categories', categoryController.getCategories)
+
+//display particularly item
+router.get('/ware/:id', wareController.getWare)
+router.get('/providor/:id', providorController.getProvidor)
+router.get('/category/:id', categoryController.getCategory)
+
+//create new item
+router.post('/ware/create', wareController.createWare)
+router.post('/providor/create', providorController.createProvidor)
+router.post('/category/create', categoryController.createCategory)
+
+//update existing item
+router.put('/ware/:id', wareController.updateWare)
+router.put('/providor/:id', providorController.updateProvidor)
+router.put('/category/:id', categoryController.updateCategory)
+
+//delete item
+router.delete('/ware/:id', wareController.deleteWare)
+router.delete('/providor/:id', providorController.deleteProvidor)
+router.delete('/category/delete/:id', categoryController.deleteCategory)
 
 module.exports = router
